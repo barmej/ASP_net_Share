dc1d7859adf5407b2756693e0e4c09bd30e77aec
Omar Alibrahim <omar.alibrahim@eng.ku.edu.kw>
2019-07-21 00:45:43 +0300

    C4-T8-V7 Last video

---
+++ Share/Areas/Admin/Controllers/RequestsController.cs
@@ -0,0 +1,49 @@
+﻿using Share.Business;
+using Share.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Share.Areas.Admin.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class RequestsController : Controller
+    {
+        // GET: Admin/Requests
+        public ActionResult Index()
+        {
+            BorrowedToolRepository borrowRepo = new BorrowedToolRepository();
+            var model = borrowRepo.GetAllBorrowedTools();
+
+            return View(model);
+        }
+
+        public ActionResult Approve(int id)
+        {
+            BorrowedToolRepository borrowRepo = new BorrowedToolRepository();
+            ToolRepository toolRepo = new ToolRepository();
+
+            BorrowedTool borrowedTool = borrowRepo.GetBorrowedToolById(id);
+            if (borrowedTool.tool_id.HasValue)
+            {
+                int toolId = borrowedTool.tool_id.Value;
+                Tool tool = toolRepo.GetToolById(toolId);
+
+                if (tool.quantity > 0)
+                {
+                    tool.quantity--;
+                    borrowedTool.approved = true;
+                    borrowRepo.Update(borrowedTool);
+                    toolRepo.Update(tool);
+                    TempData["SuccessMessage"] = "تمت العملية بنجاج";
+
+                    return RedirectToAction("Index");
+                }
+            }
+            TempData["ErrorMessage"] = "عذراً لا يمكن إكمال الطلب";
+            return RedirectToAction("Index");
+        }
+    }
+}
